16dda8b9b8cf44e62f401270b85e5683e3dd9dac
Kinekenneth48 <kinekenneth48@gmail.com>
2021-12-14 10:34:22 -0600

    large data compressed with lazy compression. Also added Rcpp documentation

--- src/calc_d4_method.cpp
+++ src/calc_d4_method.cpp
@@ -1,16 +1,40 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
+//' Calculate Day-4 Method
+//'
+//' @description Takes a numeric vector of daily snow changes, as well as
+//'   a logical vector of possible indices to include in the Day-4 method,
+//'   and returns the vector of the Day-4 method.
+//'
+//' @param dx A numeric vector of daily snow changes.
+//'
+//' @param d4_cands A logical vector of indices associated with dx.
+//'   d4_cands(i) is TRUE if and only if dx(i) and dx(i + 3) are positive,
+//'   and at most one of dx(i + 1) or dx(i + 2) are negative.
+//'
+//' @return A numeric vector of the Day-4 method. See details.
+//'
+//' @section Details:
+//' An observation of the Day-4 method consists of the sum of four
+//'   consecutive elements of dx, the first and last of which must be
+//'   positive, only one negative observation in the middle,
+//'   and the sum of the four observations must be positive.
+//'   The d4_cands logical vector is TRUE when the first and fourth
+//'   observations are positive, and at most one of the second and third
+//'   observation is negative. At the indices where d4_cands is TRUE,
+//'   this function sums the dx(i), dx(i + 1), dx(i + 2), and dx(i + 3).
+//'   If that sum is positive, it is added as a value of the Day-4 method.
+//'   The function would then falsify d4_cands(i + 1), d4_cands(i + 2),
+//'   and d4_cands(i + 3) as an index of dx can be used in a sum only once.
+//'
+//' @examples
+//' dx <- c(1, -3, 3, 1, 2, 3, 1, -7, 2, -1, -2, 3)
+//' d4_cands <- c(TRUE, FALSE, TRUE, TRUE, FALSE,
+//'               TRUE, FALSE, FALSE, TRUE)
+//' calc_d4_method(d4_cands, dx)
+//'
+//' @export
 // [[Rcpp::export]]
 NumericVector calc_d4_method(LogicalVector d4_cands,
                              NumericVector dx) {
