74e33236d5805d7605ffb4adb0fcc702e5fda474
Kinspride Duah <90093970+KinsprideDuah@users.noreply.github.com>
2021-12-14 10:06:37 -0700

    Update DAY1.R

--- R/DAY1.R
+++ R/DAY1.R
@@ -1,14 +1,16 @@
-# HELPER FUNCTION 1 #####
-# This function will take a vector x of raw snow observations. It will then
-# split the vector across n or more m's into a list of vectors.
-# > x <- c(1, 2, 0, 0, 0, 4, 5, 0, 3, 0, 0, 2, 1, 3)
-# > split_across_n_m(x)
-# $`0`
-# [1] 1 2
-# $`1`
-# [1] 4 5 0 3
-# $`2`
-# [1] 2 1 3
+#' @title split_across_n_m 
+#' @description This function takes a vector x of raw snow observations and
+#' split the vector across n or more m's into a list of vectors.
+#'
+#' @example
+#' x <- c(1, 2, 0, 0, 0, 4, 5, 0, 3, 0, 0, 2, 1, 3)
+#' split_across_n_m(x)
+#' $`0`
+#' [1] 1 2
+#' $`1`
+#' [1] 4 5 0 3
+#' $`2`
+#' [1] 2 1 3
 split_across_n_m <- function(x, n = 2, m = 0) {
   r <- rle(x == m)
   r$values <- cumsum(r$values & r$lengths > n - 1)
@@ -16,16 +18,12 @@ split_across_n_m <- function(x, n = 2, m = 0) {
   return(result)
 }
 
-
-
-
-# HELPER FUNCTION 2 #####
-# Because of HELPER FUNCTION 1, the HELPER FUNCTION 2 assumes to work with
-# raw observations with no consecutive 0's.
-
-# This function will take raw observations, make sure there is enough data to
-# perform the D1 Method (if not, it will return NULL), and then calculate
-# the D1 Method.
+#' @title d1_method_from_vector
+#' @description Helper Function 2 works in the same manner as Helper Function 1                  
+#' but this time time the vector x has no consecutive 0's. This function takes
+#' raw observations, make sure there is enough data to perform the D1 Method 
+#' (if not, it will return NULL), and then calculate the D1 Method.
+                   
 d1_method_from_vector <- function(x) {
   if (length(x) < 1) {
     return(NULL)
@@ -49,7 +47,6 @@ d1_method_from_vector <- function(x) {
 #'
 #' @return A list of numeric vectors containing the observations
 #'   for the D1 Method (list split across 2 or more raw observations of 0).
-#'
 
 d1_method <- function(df, col_name = "SWE") {
   x <- df[[col_name]]
@@ -58,11 +55,6 @@ d1_method <- function(df, col_name = "SWE") {
   result <- unlist(result) # made change
   return(result)
 }
-
-
-
-
-
 
 #' @title Extract Day-1 method observations
 #' @description This function allows the user to extract sequential daily
